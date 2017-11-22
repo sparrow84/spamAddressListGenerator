b4a71b6e773d7fcacf29ae0ff9786f3c54876247
sparrow84 <vorobey280184@mail.ru>
2017-11-22 21:31:09 +0700

    qwe

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String ... args){
-        System.out.println("qwe");
+        System.out.println("*** Start program ***");
+
+
+
+
+
+
+
+
+
+        System.out.println("*** End program ***");
     }
 }
