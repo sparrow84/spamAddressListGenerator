a74fdf376fcec019cb95db93df232fd2e74171e7
vva <vorobey280184@mail.ru>
2017-12-07 09:34:36 +0700

    проверяю работу гит клиента

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -36,6 +36,7 @@ public class Main {
                 ConfWork.chageConfig("lineNumberToStart", "1");
 
 
+
 //                System.out.println("NEW ---------------------- NEW");
 //                for (String s : DBWork.getListWithForbiddenAddresses()) {
 //                    System.out.println("   -   " + s);
